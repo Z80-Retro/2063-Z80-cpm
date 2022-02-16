4a533937bd722d2917ec10633752ea77d49714eb
John Winans <john@winans.org>
2022-02-16 15:45:10 -0600

    Hide local labels

--- boot/firmware.asm
+++ boot/firmware.asm
@@ -19,14 +19,13 @@
 ;
 ;****************************************************************************
 
-debug:		equ	0		; Set to 1 to show debug printing, else 0 
+.debug:		equ	0		; Set to 1 to show debug printing, else 0 
 
 include	'io.asm'
+include	'memory.asm'
 
-load_base:	equ	0xc000		; Where to load the boot image from the SD card.
-load_blks:	equ	(0x10000-load_base)/512
-
-stacktop:	equ	load_base	; (so the SD loader does not overwrite)
+.load_blks:	equ	(0x10000-LOAD_BASE)/512
+.stacktop:	equ	LOAD_BASE	; (so the SD loader does not overwrite)
 
 	org		0x0000		; Cold reset Z80 entry point.
 
@@ -42,7 +41,7 @@ stacktop:	equ	load_base	; (so the SD loader does not overwrite)
 	; writing it back into the same address.
 	ld	hl,0
 	ld	de,0
-	ld	bc,_end
+	ld	bc,.end
 	ldir				; Copy all the code in the FLASH into RAM at same address.
 
 	; Disable the FLASH and run from SRAM only from this point on.
@@ -52,7 +51,7 @@ stacktop:	equ	load_base	; (so the SD loader does not overwrite)
 	; STARTING HERE, WE ARE RUNNING FROM RAM
 	;###################################################
 
-	ld	sp,stacktop
+	ld	sp,.stacktop
 
 	; Initialize the CTC so that the SIO will have a baud clock if J11-A is set to the CTC!
 	;ld	c,1			; 115200 bps
@@ -63,73 +62,67 @@ stacktop:	equ	load_base	; (so the SD loader does not overwrite)
 	call	sioa_init
 
 	; Display a hello world message.
-	ld	hl,boot_msg
+	ld	hl,.boot_msg
 	call	puts
 
 	; Load bootstrap code from the SD card.
-	call	boot_sd
+	call	.boot_sd
 
-	ld	hl,done_msg
-	call	puts
+	call	iputs
+	db	'SYSTEM LOAD FAILED! HALTING.\r\n\0'
 
 	; Spin loop here because there is nothing else to do
-halt_loop:
+.halt_loop:
 	halt
-	jp	halt_loop
+	jp	.halt_loop
 
-
-boot_msg:
-	defb    '\r\n\n'
-	defb	'##############################################################################\r\n'
-	defb	'Z80 Retro Board 2063.3\r\n'
-	defb	'      git: @@GIT_VERSION@@\r\n'
-	defb	'    build: @@DATE@@\r\n'
-	defb	'\0'
-
-done_msg:
-	defb	'SYSTEM LOAD FAILED! HALTING.\r\n\0'
-
-
+.boot_msg:
+	db	'\r\n\n'
+	db	'##############################################################################\r\n'
+	db	'Z80 Retro Board 2063.3\r\n'
+	db	'      git: @@GIT_VERSION@@\r\n'
+	db	'    build: @@DATE@@\r\n'
+	db	'\0'
 
 ;##############################################################################
 ; Load 16K from the first blocks of partition 1 on the SD card into
-; memory starting at 'load_base' and jump to it.
+; memory starting at 'LOAD_BASE' and jump to it.
 ; If reading the SD card should fail then this function will return.
 ;
 ; TODO: Sanity-check the partition type, size and design some sort of 
 ; signature that can be used to recognize the SD card partition as viable.
 ;##############################################################################
-boot_sd:
+.boot_sd:
 	call	iputs
-	db		'\r\nBooting SD card partition 1\r\n\n\0'
+	db	'\r\nBooting SD card partition 1\r\n\n\0'
 
 	call	sd_boot		; transmit 74+ CLKs
 
 	; The response byte should be 0x01 (idle) from cmd0
 	call	sd_cmd0
 	cp	0x01
-	jr	z,boot_sd_1
+	jr	z,.boot_sd_1
 
 	call	iputs
 	db	'Error: Can not read SD card (cmd0 command status not idle)\r\n\0'
 	ret
 
-boot_sd_1:
-	ld	de,load_base	; Use the load area for a temporary buffer
+.boot_sd_1:
+	ld	de,LOAD_BASE	; Use the load area for a temporary buffer
 	call	sd_cmd8		; CMD8 is sent to verify that we have a Version 2+ SD card
 				; and agree on the operating voltage.
 				; CMD8 also expands the functionality of CMD58 and ACMD41
 
 	; The response should be: 0x01 0x00 0x00 0x01 0xAA.
-	ld	a,(load_base)
+	ld	a,(LOAD_BASE)
 	cp	1
-	jr	z,boot_sd_2
+	jr	z,.boot_sd_2
 
 	call	iputs
 	db	'Error: Can not read SD card (cmd8 command status not valid):\r\n\0'
 
 	; dump the command response buffer
-	ld	hl,load_base	; dump bytes from here
+	ld	hl,LOAD_BASE	; dump bytes from here
 	ld	e,0		; no fancy formatting
 	ld	bc,5		; dump 5 bytes
 	call	hexdump
@@ -138,47 +131,48 @@ boot_sd_1:
 	ret
 
 
-boot_sd_2:
+.boot_sd_2:
 
 ; After power cycle, card is in 3.3V signaling mode.  We do not intend 
 ; to change it nor we we care about what other options may be available.
 ; XXX I don't care what voltages are supported... I assume that 3.3v is fine.
-;	ld	de,load_base
+;	ld	de,LOAD_BASE
 ;	call	sd_cmd58		; cmd58 = read OCR (operation conditions register)
 
-ac41_max_retry:	equ	0x80		; limit the number of acmd41 retries
+.ac41_max_retry: equ	0x80		; limit the number of acmd41 retries
 
-	ld	b,ac41_max_retry
-ac41_loop:
+	ld	b,.ac41_max_retry
+.ac41_loop:
 	push	bc			; save BC since B contains the retry count 
-	ld	de,load_base		; store command response into load_base
+	ld	de,LOAD_BASE		; store command response into LOAD_BASE
 	call	sd_acmd41		; ask if the card is ready
 	pop	bc			; restore our retry counter
 	or	a			; check to see if A is zero
-	jr	z,ac41_done		; is A is zero, then the card is ready
+	jr	z,.ac41_done		; is A is zero, then the card is ready
 
 	; Card is not ready, waste some time before trying again
 	ld	hl,0x1000		; count to 0x1000 to consume time
-ac41_dly:
+.ac41_dly:
 	dec	hl			; HL = HL -1
 	ld	a,h			; does HL == 0?
 	or	l
-	jr	nz,ac41_dly		; if HL != 0 then keep counting
+	jr	nz,.ac41_dly		; if HL != 0 then keep counting
 
-	djnz	ac41_loop		; if (--retries != 0) then try again
+	djnz	.ac41_loop		; if (--retries != 0) then try again
 
 
-ac41_fail:
+.ac41_fail:
 	call	iputs
 	db	'Error: Can not read SD card (ac41 command failed)\r\n\0'
 	ret
 
-ac41_done:
-if 0
+.ac41_done:
+if .debug
 	call	iputs
 	db	'** Note: Called ACMD41 0x\0'
 	ld	a,ac41_max_retry
 	sub	b
+	inc	a			; account for b not yet decremented on last time
 	call	hexdump_a
 	call	iputs
 	db	' times.\r\n\0'
@@ -186,70 +180,77 @@ endif
 
 	; Find out the card capacity (HC or XC)
 	; This status is not valid until after ACMD41.
-	ld	de,load_base
+	ld	de,LOAD_BASE
 	call	sd_cmd58
 
-if 0
+if .debug
 	call	iputs
-	db	'** Note: Called CMD58: \0'
-	ld	hl,load_base
+	db	'** Note: Called CMD58: R3: \0'
+	ld	hl,LOAD_BASE
 	ld	bc,5
 	ld	e,0
 	call	hexdump
 endif
 
 	; Check that CCS=1 here to indicate that we have an HC/XC card
-	ld	a,(load_base+1)
+	ld	a,(LOAD_BASE+1)
 	and	0x40			; CCS bit is here (See spec p275)
-	jr	nz,boot_hcxc_ok
+	jr	nz,.boot_hcxc_ok
 
 	call	iputs
 	db	'Error: SD card capacity is not HC or XC.\r\n\0'
 	ret
 
 
-boot_hcxc_ok:
+.boot_hcxc_ok:
 	; ############ Read the MBR ############
 
 	; push the starting block number onto the stack in little-endian order
 	ld	hl,0			; SD card block number to read
 	push	hl			; high half
 	push	hl			; low half
-	ld	de,load_base		; where to read the sector data into
+	ld	de,LOAD_BASE		; where to read the sector data into
 	call	sd_cmd17
 	pop	hl			; remove the block number from the stack
 	pop	hl
 
-if debug
-	call	puts_crlf		; skip a line to set off from any debug dumps
-endif
-if 1
+	or	a
+	jr	z,.boot_cmd17_ok
+
+	call	iputs
+	db	'Error: SD card CMD17 failed to read block zero.\r\n\0'
+	ret
+
+.boot_cmd17_ok:
+if 1 ;.debug
 	call	iputs
 	db	'Partition Table:\r\n\0'
 
-	ld	hl,load_base+0x01BE	; address of the first partiton entry
+	ld	hl,LOAD_BASE+0x01BE	; address of the first partiton entry
 	ld	e,0			; no fancy formatting
 	ld	bc,16			; dump 16 bytes
 	call	hexdump
-	ld	hl,load_base+0x01CE	; address of the second partiton entry
+	ld	hl,LOAD_BASE+0x01CE	; address of the second partiton entry
 	ld	e,0			; no fancy formatting
 	ld	bc,16			; dump 16 bytes
 	call	hexdump
-	ld	hl,load_base+0x01DE	; address of the third partiton entry
+	ld	hl,LOAD_BASE+0x01DE	; address of the third partiton entry
 	ld	e,0			; no fancy formatting
 	ld	bc,16			; dump 16 bytes
 	call	hexdump
-	ld	hl,load_base+0x01EE	; address of the fourth partiton entry
+	ld	hl,LOAD_BASE+0x01EE	; address of the fourth partiton entry
 	ld	e,0			; no fancy formatting
 	ld	bc,16			; dump 16 bytes
 	call	hexdump
 endif
 
-	; Find the geometry of the first partition record:
-	ld	ix,load_base+0x01BE+0x08
+	; XXX validate that we really HAVE an MBR and that it looks OK to boot! XXX
 
-	ld	hl,boot_sd_start
-	call	puts
+	; Find the geometry of the first partition record:
+	ld	ix,LOAD_BASE+0x01BE+0x08
+
+	call	iputs
+	db	'\nPartition 1 starting block number: \0'
 	ld	a,(ix+3)
 	call	hexdump_a
 	ld	a,(ix+2)
@@ -260,8 +261,8 @@ endif
 	call	hexdump_a
 	call	puts_crlf
 
-	ld	hl,boot_sd_count
-	call	puts
+	call	iputs
+	db	'Partition 1 number of blocks:      \0'
 	ld	a,(ix+7)
 	call	hexdump_a
 	ld	a,(ix+6)
@@ -274,7 +275,7 @@ endif
 
 
 	; ############ Read the first sectors of the first partition ############
-	ld	ix,load_base+0x01BE+0x08
+	ld	ix,LOAD_BASE+0x01BE+0x08
 	ld	d,(ix+3)
 	ld	e,(ix+2)
 	push	de
@@ -282,27 +283,27 @@ endif
 	ld	e,(ix+0)
 	push	de
 
-	ld	de,load_base		; where to read the sector data into
-	ld	b,load_blks		; number of blocks to load (should be 32/16KB)
+	ld	de,LOAD_BASE		; where to read the sector data into
+	ld	b,.load_blks		; number of blocks to load (should be 32/16KB)
 
 if 1
 	; Print the details of what we are going to load and where it will go
-	ld	hl,sd_msg_sd_blks1
-	call	puts
+	call	iputs
+	db	'\nLoading 0x\0'
 	ld	a,b
 	call	hexdump_a
-	ld	hl,sd_msg_sd_blks2
-	call	puts
+	call	iputs
+	db	' 512-byte blocks into 0x\0'
 	ld	a,d
 	call	hexdump_a
 	ld	a,e
 	call	hexdump_a
-	ld	hl,sd_msg_sd_blks3
-	call	puts
+	call	iputs
+	db	' - 0x\0'
 
 	; Calculate the ending address of the load area
-	ld	hl,load_base
-	ld	a,load_blks
+	ld	hl,LOAD_BASE
+	ld	a,.load_blks
 	add	a
 	ld	b,a
 	ld	c,0
@@ -315,25 +316,20 @@ if 1
 	call	puts_crlf
 
 	; re-load these if the debug logic messed them up
-	ld	de,load_base		; where to read the sector data into
-	ld	b,load_blks		; number of blocks to load (should be 32/16KB)
+	ld	de,LOAD_BASE		; where to read the sector data into
+	ld	b,.load_blks		; number of blocks to load (should be 32/16KB)
 endif
 
 	call	read_blocks
 	pop	de			; Remove the 32-bit block number from the stack.
 	pop	de
 
-	jp	load_base		; Run the code that we just read in from the SD card.
+	or	a
+	jp	z,LOAD_BASE		; Run the code that we just read in from the SD card.
 
-
-boot_sd_start:		defb	'\nPartition 1 starting block number: \0'
-boot_sd_count:		defb	'Partition 1 number of blocks:      \0'
-
-boot_sd_agn:		defb	'Reinitialize and read SD card again? (x to quit)\0'
-
-sd_msg_sd_blks1:	defb	'\nLoading 0x\0'
-sd_msg_sd_blks2:	defb	' 512-byte blocks into 0x\0'
-sd_msg_sd_blks3:	defb	' - 0x\0'
+	call	iputs
+	db	'Error: Could not load O/S from partition 0.\r\n\0'
+	ret
 
 
 
@@ -365,7 +361,7 @@ read_blocks:
 
 	;call	spi_read8f_init for the 8f version of CMD17
 
-read_block_n:
+.read_block_n:
 
 if 1
 	ld	c,'.'
@@ -373,8 +369,9 @@ if 1
 endif
 
 if 0
-	ld	hl,rb_blk_msg
-	call	puts
+	call	iputs
+	db	'Read Block: \0'
+
 	ld	a,(iy+3)
 	call	hexdump_a
 	ld	a,(iy+2)
@@ -389,11 +386,11 @@ endif
 	; SP is currently pointing at the block number
 	call	sd_cmd17
 	or	a
-	jr	nz,rb_fail
+	jr	nz,.rb_fail
 
 	; count the block
 	dec	b
-	jr	z,rb_success		; note that a=0 here = success!
+	jr	z,.rb_success		; note that a=0 here = success!
 
 	; increment the target address by 512
 	inc	d
@@ -401,18 +398,18 @@ endif
 
 	; increment the 32-bit block number
 	inc	(iy+0)
-	jr	nz,read_block_n
+	jr	nz,.read_block_n
 	inc	(iy+1)
-	jr	nz,read_block_n
+	jr	nz,.read_block_n
 	inc	(iy+2)
-	jr	nz,read_block_n
+	jr	nz,.read_block_n
 	inc	(iy+3)
-	jr	read_block_n
+	jr	.read_block_n
 
-rb_success:
+.rb_success:
 	xor	a
 
-rb_fail:
+.rb_fail:
 	ld	iy,4
 	add	iy,sp
 	ld	sp,iy
@@ -420,9 +417,6 @@ rb_fail:
 	pop	de
 	pop	bc
 	ret
-
-
-rb_blk_msg:	defb	'Read Block: \0'
 
 
 include	'sdcard.asm'
@@ -442,4 +436,4 @@ gpio_out_cache:	db	gpio_out_sd_mosi|gpio_out_sd_ssel|gpio_out_prn_stb
 ;##############################################################################
 ; This marks the end of the data copied from FLASH into RAM during boot
 ;##############################################################################
-_end:		equ	$
+.end:		equ	$
